699f1924246c8b317e7ad65d1bf6a3d57504db2d
ebtaterbug <ityjoseph@gmail.com>
2022-07-28 19:55:28 -0700

    fix reports filtering

--- public/javascript/reports.js
+++ public/javascript/reports.js
@@ -6,22 +6,23 @@ function timeConverter(UNIX_timestamp){
     var date = a.getDate();
     var hour = a.getHours();
     var min = a.getMinutes();
-    var sec = a.getSeconds();
-    if (min < 10) {
-        var time = date + ' ' + month + ' ' + year + ' ' + hour + ':' + '0' + min
-    } else {
-        var time = date + ' ' + month + ' ' + year + ' ' + hour + ':' + min
-    }
+
+    
+    var time = date + ' ' + month + ' ' + year + ' ' + `${(hour<=12) ? hour : hour-12}` + ':' + `${(min<10) ? '0' : ''}` + min + `${(hour<12) ? ' AM' : ' PM'}`
+
     return time;
 }
 
-
 async function getReports(e) {
     e.preventDefault()
-
+    document.querySelector(".reports").innerHTML = ''
+    
     const device = document.querySelector('#device').value.trim()
-    const startDate = Date.parse(document.querySelector('.start-date').value.trim() + ' PST')
-    const endDate = Date.parse(document.querySelector('.end-date').value.trim() + '  PST')
+    let startDate = Date.parse(document.querySelector('.start-date').value.trim())
+    let endDate = Date.parse(document.querySelector('.end-date').value.trim())
+
+     startDate = startDate+25200000
+     endDate = endDate+25200000
 
     let response = await fetch(`https://flespi.io/gw/devices/${device}/messages?data=%7B%22from%22%3A${startDate.toString().substr(0, startDate.toString().length - 3)}%2C%22to%22%3A${endDate.toString().substr(0, endDate.toString().length - 3)}%7D`, {
       method: 'GET',
@@ -37,12 +38,13 @@ async function getReports(e) {
     .map(report => {
         return `
         <div class="px-2 my-2 w-100 d-flex justify-content-between border">
-            <p class="m-0 small">${timeConverter(report.timestamp)} PST</p>
+            <p class="m-0 small">${timeConverter(report.timestamp)}</p>
             <a class="m-0 small" target="_blank" href="https://www.google.com/maps/search/${report["position.latitude"] || ''},${report["position.longitude"] || ''}">${report["position.latitude"] || ''}, ${report["position.longitude"] || ''}</a>
             <p class="m-0 small">${(((report['ble.sensor.temperature.1']) * 1.8) + 32).toFixed(1)} °F</p>
         </div>
         `
     }).join("")
+
     document.querySelector(".reports").insertAdjacentHTML("afterbegin", html)
 }
 
